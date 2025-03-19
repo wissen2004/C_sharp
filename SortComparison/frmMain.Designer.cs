6e19794fb72df559c385f2cb51ef2a5a49eef528
Vlad <satogob75@gmail.com>
2025-03-19 14:10:36 +0600

    first commit

---
+++ SortComparison/frmMain.Designer.cs
@@ -0,0 +1,308 @@
+﻿using System.Threading;
+
+namespace SortComparison
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboAlg1 = new System.Windows.Forms.ComboBox();
+            this.cboAlg2 = new System.Windows.Forms.ComboBox();
+            this.cmdShuffle = new System.Windows.Forms.Button();
+            this.cmdSort = new System.Windows.Forms.Button();
+            this.pnlSort1 = new System.Windows.Forms.PictureBox();
+            this.pnlSort2 = new System.Windows.Forms.PictureBox();
+            this.tbSamples = new System.Windows.Forms.TrackBar();
+            this.lblSamples = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbSpeed = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkAnimation = new System.Windows.Forms.CheckBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.txtOutputFolder = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlSort1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlSort2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSamples)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cboAlg1
+            // 
+            this.cboAlg1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboAlg1.FormattingEnabled = true;
+            this.cboAlg1.Items.AddRange(new object[] {
+            "",
+            "Двунаправленная пузырьковая сортировка",
+            "Пузырьковая сортировка",
+            "Карманная сортировка",
+            "Гребенчатая сортировка",
+            "Циклическая сортировка",
+            "Гномья сортировка",
+            "Пирамидальная сортировка",
+            "Сортировка вставками",
+            "Сортировка слиянием",
+            "Нечётно-чётная сортировка",
+            "Сортировка методом голубятни",
+            "Быстрая сортировка",
+            "Быстрая сортировка с пузырьковой сортировкой",
+            "Сортировка выбором",
+            "Сортировка Шелла"});
+            this.cboAlg1.Location = new System.Drawing.Point(-3, 482);
+            this.cboAlg1.Name = "cboAlg1";
+            this.cboAlg1.Size = new System.Drawing.Size(568, 33);
+            this.cboAlg1.TabIndex = 2;
+            // 
+            // cboAlg2
+            // 
+            this.cboAlg2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cboAlg2.FormattingEnabled = true;
+            this.cboAlg2.Items.AddRange(new object[] {
+            "",
+            "Двунаправленная пузырьковая сортировка",
+            "Пузырьковая сортировка",
+            "Карманная сортировка",
+            "Гребенчатая сортировка",
+            "Циклическая сортировка",
+            "Гномья сортировка",
+            "Пирамидальная сортировка",
+            "Сортировка вставками",
+            "Сортировка слиянием",
+            "Нечётно-чётная сортировка",
+            "Сортировка методом голубятни",
+            "Быстрая сортировка",
+            "Быстрая сортировка с пузырьковой сортировкой",
+            "Сортировка выбором",
+            "Сортировка Шелла"});
+            this.cboAlg2.Location = new System.Drawing.Point(577, 482);
+            this.cboAlg2.Name = "cboAlg2";
+            this.cboAlg2.Size = new System.Drawing.Size(568, 33);
+            this.cboAlg2.TabIndex = 3;
+            // 
+            // cmdShuffle
+            // 
+            this.cmdShuffle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdShuffle.Location = new System.Drawing.Point(700, 527);
+            this.cmdShuffle.Name = "cmdShuffle";
+            this.cmdShuffle.Size = new System.Drawing.Size(92, 41);
+            this.cmdShuffle.TabIndex = 4;
+            this.cmdShuffle.Text = "Создать";
+            this.cmdShuffle.UseVisualStyleBackColor = true;
+            this.cmdShuffle.Click += new System.EventHandler(this.cmdShuffle_Click);
+            // 
+            // cmdSort
+            // 
+            this.cmdSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdSort.Location = new System.Drawing.Point(834, 527);
+            this.cmdSort.Name = "cmdSort";
+            this.cmdSort.Size = new System.Drawing.Size(129, 41);
+            this.cmdSort.TabIndex = 5;
+            this.cmdSort.Text = "Сортировать";
+            this.cmdSort.UseVisualStyleBackColor = true;
+            this.cmdSort.Click += new System.EventHandler(this.cmdSort_Click);
+            // 
+            // pnlSort1
+            // 
+            this.pnlSort1.BackColor = System.Drawing.Color.White;
+            this.pnlSort1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSort1.Location = new System.Drawing.Point(-3, 0);
+            this.pnlSort1.Name = "pnlSort1";
+            this.pnlSort1.Size = new System.Drawing.Size(568, 476);
+            this.pnlSort1.TabIndex = 6;
+            this.pnlSort1.TabStop = false;
+            // 
+            // pnlSort2
+            // 
+            this.pnlSort2.BackColor = System.Drawing.Color.White;
+            this.pnlSort2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlSort2.Location = new System.Drawing.Point(577, 0);
+            this.pnlSort2.Name = "pnlSort2";
+            this.pnlSort2.Size = new System.Drawing.Size(568, 476);
+            this.pnlSort2.TabIndex = 7;
+            this.pnlSort2.TabStop = false;
+            // 
+            // tbSamples
+            // 
+            this.tbSamples.Location = new System.Drawing.Point(405, 537);
+            this.tbSamples.Maximum = 100;
+            this.tbSamples.Minimum = 10;
+            this.tbSamples.Name = "tbSamples";
+            this.tbSamples.Size = new System.Drawing.Size(256, 45);
+            this.tbSamples.SmallChange = 10;
+            this.tbSamples.TabIndex = 8;
+            this.tbSamples.TickFrequency = 10;
+            this.tbSamples.Value = 20;
+            // 
+            // lblSamples
+            // 
+            this.lblSamples.AutoSize = true;
+            this.lblSamples.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSamples.Location = new System.Drawing.Point(166, 538);
+            this.lblSamples.Name = "lblSamples";
+            this.lblSamples.Size = new System.Drawing.Size(233, 25);
+            this.lblSamples.TabIndex = 9;
+            this.lblSamples.Text = "Number of samples: 20";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(164, 587);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Sorting speed:";
+            // 
+            // tbSpeed
+            // 
+            this.tbSpeed.Location = new System.Drawing.Point(405, 577);
+            this.tbSpeed.Maximum = 100;
+            this.tbSpeed.Name = "tbSpeed";
+            this.tbSpeed.Size = new System.Drawing.Size(256, 45);
+            this.tbSpeed.SmallChange = 10;
+            this.tbSpeed.TabIndex = 11;
+            this.tbSpeed.TickFrequency = 10;
+            this.tbSpeed.Value = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(371, 581);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 20);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Min";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(659, 581);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Max";
+            // 
+            // chkAnimation
+            // 
+            this.chkAnimation.AutoSize = true;
+            this.chkAnimation.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkAnimation.Location = new System.Drawing.Point(746, 607);
+            this.chkAnimation.Name = "chkAnimation";
+            this.chkAnimation.Size = new System.Drawing.Size(173, 28);
+            this.chkAnimation.TabIndex = 14;
+            this.chkAnimation.Text = "Создать анимацию";
+            this.chkAnimation.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectFolder.Location = new System.Drawing.Point(730, 648);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(62, 26);
+            this.btnSelectFolder.TabIndex = 17;
+            this.btnSelectFolder.Text = "...";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // txtOutputFolder
+            // 
+            this.txtOutputFolder.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutputFolder.Location = new System.Drawing.Point(370, 646);
+            this.txtOutputFolder.Name = "txtOutputFolder";
+            this.txtOutputFolder.Size = new System.Drawing.Size(335, 26);
+            this.txtOutputFolder.TabIndex = 16;
+            this.txtOutputFolder.Text = "C:\\Users\\ASUS\\Desktop\\3kurs\\c_scharp\\anim\\";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(165, 646);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 25);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Папка:";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1146, 797);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.txtOutputFolder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.chkAnimation);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbSpeed);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSamples);
+            this.Controls.Add(this.tbSamples);
+            this.Controls.Add(this.pnlSort2);
+            this.Controls.Add(this.pnlSort1);
+            this.Controls.Add(this.cmdSort);
+            this.Controls.Add(this.cmdShuffle);
+            this.Controls.Add(this.cboAlg2);
+            this.Controls.Add(this.cboAlg1);
+            this.Name = "frmMain";
+            this.Text = "Сравнение сортировок";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pnlSort1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pnlSort2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSamples)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbSpeed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboAlg1;
+        private System.Windows.Forms.ComboBox cboAlg2;
+        private System.Windows.Forms.Button cmdShuffle;
+        private System.Windows.Forms.Button cmdSort;
+        private System.Windows.Forms.PictureBox pnlSort1;
+        private System.Windows.Forms.PictureBox pnlSort2;
+        private System.Windows.Forms.TrackBar tbSamples;
+        private System.Windows.Forms.Label lblSamples;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar tbSpeed;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkAnimation;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.TextBox txtOutputFolder;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
